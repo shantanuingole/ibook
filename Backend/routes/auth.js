ec60e2a861e91354df2dab9ce417ad39b9fcdc4a
shantanuingole <shantanuingole23@gmail.com>
2025-07-20 11:39:13 +0530

    added new files

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { body, validationResult } = require('express-validator');
 const router = express.Router();
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 
 router.post('/createuser' ,
@@ -24,9 +25,12 @@ router.post('/createuser' ,
     return res.status(400).json({error : "sorry email already exist !"})
    }
 
+   const salt = await bcrypt.genSalt(10) ;
+   const secPass = await bcrypt.hash(req.body.password , salt) ;
+
      user = await User.create({
       name: req.body.name,
-      password: req.body.password,
+      password: secPass,
        email: req.body.email,
     })
 res.json(user) ;
